2fbc48b43e6ba53e10c368a9309c2fa436f3d9a9
Stepan Saprunov <saprunovstepan.0@gmail.com>
2023-09-12 18:33:45 +0300

    initial commit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { UsersModule } from "./users/users.module";
+import { User } from "./users/users.model";
+
+@Module({
+  controllers: [],
+  providers: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: "postgres",
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DATABASE,
+      autoLoadModels: true,
+      models: [User],
+    }),
+    UsersModule,
+  ],
+})
+export class AppModule {}
---
+++ backend/src/users/users.model.ts
@@ -0,0 +1,32 @@
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+
+interface UserCreationAttr {
+  username: string;
+  password: string;
+}
+
+@Table({
+  tableName: "users",
+})
+export class User extends Model<User, UserCreationAttr> {
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+
+  @Column({
+    type: DataType.STRING,
+    unique: true,
+    allowNull: false,
+  })
+  username: string;
+
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  password: string;
+}
